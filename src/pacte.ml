bdbbbc4a9dde4d24786aad04f46ec586b4649ff5
Arthur Correnson <arthur.correnson@gmail.com>
2021-01-20 15:58:19 +0100

    Error reporting

--- src/pacte.ml
+++ src/pacte.ml
@@ -1,22 +1,50 @@
-type 'a parser = string -> (string * 'a)
 
-exception ParseFail
+type 'a result =
+  | Ok of string * 'a
+  | Ko of string list
+
+type 'a parser = string -> 'a result
 
 let rem s =
   String.(sub s 1 ((length s) - 1))
 
-let satisfy (pred : char -> bool) : char parser = fun input ->
+let satisfy (name : string) (pred : char -> bool) : char parser = fun input ->
   match input with
-  | "" -> failwith "no more characters"
+  | "" -> Ko [name; "End of buffer"]
   | _ ->
-    if pred input.[0] then rem input, input.[0]
-    else raise ParseFail
+    if pred input.[0] then Ok (rem input, input.[0])
+    else Ko [name]
 
-let char (c : char) = satisfy ((=) c)
+let char (c : char) = satisfy ("char " ^ String.make 1 c) ((=) c)
 
 let (<|>) (p1 : 'a parser) (p2 : 'a parser) : 'a parser = fun input ->
-  try p1 input
-  with ParseFail -> p2 input
+  match p1 input with
+  | Ok _ as res -> res
+  | Ko _ -> p2 input
 
+let (<?>) name p = fun inp ->
+  match p inp with
+  | Ok _ as res -> res
+  | Ko l -> Ko (name::l)
+
+let rec many1 p = fun inp ->
+  match p inp with
+  | Ko _ as k -> k
+  | Ok (next, res) ->
+    match many1 p next with
+    | Ok (next', res') -> Ok (next', res::res')
+    | Ko _ as k -> k
+
+let run p inp =
+  match p inp with
+  | Ko l ->
+    List.iter (Printf.printf "Error @ %s\n") l;
+    exit (-1)
+  | Ok (n, r) -> n, r
+
+let main () =
+  let p = "a | b" <?> (char 'a' <|> char 'b') in
+  let q = "(a | b)*" <?> (many1 p) in
+  run q "cba"
 
 
--- src/pacte.mli
+++ src/pacte.mli
@@ -1,7 +1,14 @@
+type 'a result =   | Ok of string * 'a
+                   | Ko of string list
+
 type 'a parser
 
-val satisfy : (char -> bool) -> char parser
+val satisfy : string -> (char -> bool) -> char parser
 
 val char : char -> char parser
 
 val (<|>) : char parser -> char parser -> char parser
+
+val many1 : 'a parser -> 'a list parser
+
+val main : unit -> string * char list
